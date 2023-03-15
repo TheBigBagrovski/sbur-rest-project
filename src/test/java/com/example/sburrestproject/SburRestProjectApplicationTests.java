701d605d91f52d5bf09886f8248f819fd8f32c3a
TheBigBagrovski <matvey.bagr77@mail.ru>
2023-03-15 22:45:49 +0300

    Initial commit

---
+++ src/test/java/com/example/sburrestproject/SburRestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sburrestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SburRestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
